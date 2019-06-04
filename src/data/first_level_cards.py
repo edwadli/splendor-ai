36a42a66b898e1a911a21986ae4e6498f188da5d
erikhou45 <erikhou0922@gmail.com>
2019-06-03 22:12:39 -0700

    add csv files for development cards and update card building modules

--- src/data/first_level_cards.py
+++ src/data/first_level_cards.py
@@ -1,14 +1,17 @@
 from src.proto.deck_proto import Deck
 from src.proto.development_card_proto import DevelopmentCard
 from src.proto.gem_proto import GemType
+from src.data.deck_building_funcs import build_deck
 
 # TODO: fill in with actual first level card values
-FIRST_LEVEL_CARDS = [
-	DevelopmentCard(
-		asset_id="100",
-		level=Deck.LEVEL_1,
-		points=1,
-		gem=GemType.BLUE,
-		cost={GemType.RED: 4},
-	),
-]
+# FIRST_LEVEL_CARDS = [
+# 	DevelopmentCard(
+# 		asset_id="100",
+# 		level=Deck.LEVEL_1,
+# 		points=1,
+# 		gem=GemType.BLUE,
+# 		cost={GemType.RED: 4},
+# 	),
+# ]
+
+FIRST_LEVEL_CARDS = build_deck('./src/data/first_level_cards.csv')
